6ea33231ceccb9cdb7deb553c6ef727a4e009bac
Monsaw <lawalmonsaw@gmail.com>
2022-01-14 16:10:22 +0100

    secret key update...

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -24,7 +24,7 @@ MEDIA_DIR = BASE_DIR / 'media'
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g_82%6hgvc&a6u#x*0^ef_eu4bsmc6=2-8b76=)d+#e=h%wo@('
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
